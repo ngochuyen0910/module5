221fcc076842d1546304c6e5ac2154c9730d6903
ngochuyen0910 <truongngochuyen1997@gmail.com>
2022-08-15 19:55:45 +0700

    up

--- Module-5/furama-case-study/src/app/app.module.ts
+++ Module-5/furama-case-study/src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./component/header/header.component";
 import { BodyComponent } from './component/body/body.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { ListCustomerComponent } from './customer/list-customer/list-customer.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+import { ListFacilityComponent } from './facility/list-facility/list-facility.component';
+import { CreateFacilityComponent } from './facility/create-facility/create-facility.component';
+import { EditFacilityComponent } from './facility/edit-facility/edit-facility.component';
+import { ListContractComponent } from './contract/list-contract/list-contract.component';
+import { CreateContractComponent } from './contract/create-contract/create-contract.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +21,14 @@ import { FooterComponent } from './component/footer/footer.component';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
+    ListCustomerComponent,
+    EditCustomerComponent,
+    CreateCustomerComponent,
+    ListFacilityComponent,
+    CreateFacilityComponent,
+    EditFacilityComponent,
+    ListContractComponent,
+    CreateContractComponent,
   ],
   imports: [
     BrowserModule,
